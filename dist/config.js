42be4aefc8f04446ed9868a3329c33a9a8b38dbb
caquijano <carlosquijano98@gmail.com>
2021-05-20 12:11:27 -0500

    borrar dist

--- dist/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    MONGO_DATABASE: 'nomina',
-    MONGO_USER: 'admin',
-    MONGO_PASSWORD: 'admin',
-    MONGO_HOST: 'admin:admin@ikhodi.kmdva.mongodb.net',
-    SECRET: 'ikhodi'
-};
